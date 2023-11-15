1cb8cad7c9e34c5e3660728d4cb23b79d3d90862
hommaryota <ryo4111016@gmail.com>
2023-11-16 01:04:40 +0900

    BgImageコンポーネントをルートfont-size自動計算に合わせて修正

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,13 @@ import Title from "./components/title/Title";
 
 const App: React.FC = () => {
   useEffect(() => {
-    const handleResize = () => {
-      fontSet();
-    };
-    window.addEventListener("resize", handleResize);
-    window.addEventListener("load", handleResize);
+    window.addEventListener("resize", () => fontSet());
+    window.addEventListener("load", () => fontSet());
 
     // Cleanup event listener on component unmount
     return () => {
-      window.removeEventListener("resize", handleResize);
-      window.removeEventListener("load", handleResize);
+      window.removeEventListener("resize", () => fontSet());
+      window.removeEventListener("load", () => fontSet());
     };
   }, []);
 
@@ -22,25 +19,20 @@ const App: React.FC = () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
     const ratio = width / height;
-    const htmlElement = document.querySelector("html");
-    console.log(
-      (htmlElement!.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw")
-    );
+    const htmlElement = document.querySelector("html")!;
 
     if (ratio > 1.6) {
-      htmlElement!.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw";
+      htmlElement.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw";
     } else if (ratio < 1.6) {
-      htmlElement!.style.fontSize =
+      htmlElement.style.fontSize =
         Math.floor((1 / (height * 0.016)) * 10000) / 10000 + "vw";
     }
   };
 
   return (
-    <div className="square">
-      <div className={s.wrap}>
-        <BgImage />
-        <Title />
-      </div>
+    <div className={s.wrap}>
+      <BgImage />
+      <Title />
     </div>
   );
 };
