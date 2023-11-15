d28e1579d3767ab185821d189ef05e5176b22a63
hommaryota <ryo4111016@gmail.com>
2023-11-15 21:01:15 +0900

    ルートfont-size自動計算機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
+import {useEffect} from "react";
+import s from "./App.module.scss";
 import BgImage from "./components/bgImage/BgImage";
+import Title from "./components/title/Title";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    const handleResize = () => {
+      fontSet();
+    };
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("load", handleResize);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("load", handleResize);
+    };
+  }, []);
+
+  const fontSet = () => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const ratio = width / height;
+    const htmlElement = document.querySelector("html");
+    console.log(
+      (htmlElement!.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw")
+    );
+
+    if (ratio > 1.6) {
+      htmlElement!.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw";
+    } else if (ratio < 1.6) {
+      htmlElement!.style.fontSize =
+        Math.floor((1 / (height * 0.016)) * 10000) / 10000 + "vw";
+    }
+  };
+
   return (
-    <>
-      <BgImage />
-    </>
+    <div className="square">
+      <div className={s.wrap}>
+        <BgImage />
+        <Title />
+      </div>
+    </div>
   );
 };
 
