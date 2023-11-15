49e06dc4af1a99f0b0864cdf9ef3acff24d943cd
hommaryota <ryo4111016@gmail.com>
2023-11-16 00:30:47 +0900

    タイトルコンポーネント作成

--- src/components/bgImage/BgImage.tsx
+++ src/components/bgImage/BgImage.tsx
@@ -4,22 +4,22 @@ const BgImage: React.FC = () => {
   return (
     <div className={s.bg}>
       <div className={`${s.bgItem} ${s.bgItem01}`}>
-        <img src="../../assets/bg/bg-item01.svg" />
+        <img src="/assets/bg/bg-item01.svg" />
       </div>
       <div className={`${s.bgItem} ${s.bgItem02}`}>
-        <img src="../../assets/bg/bg-item02.svg" />
+        <img src="/assets/bg/bg-item02.svg" />
       </div>
       <div className={`${s.bgItem} ${s.bgItem03}`}>
-        <img src="../../assets/bg/bg-item03.svg" />
+        <img src="/assets/bg/bg-item03.svg" />
       </div>
       <div className={`${s.bgItem} ${s.bgItem04}`}>
-        <img src="../../assets/bg/bg-item04.svg" />
+        <img src="/assets/bg/bg-item04.svg" />
       </div>
       <div className={`${s.bgItem} ${s.bgItem05}`}>
-        <img src="../../assets/bg/bg-item05.svg" />
+        <img src="/assets/bg/bg-item05.svg" />
       </div>
       <div className={`${s.bgItem} ${s.bgItem06}`}>
-        <img src="../../assets/bg/bg-item06.svg" />
+        <img src="/assets/bg/bg-item06.svg" />
       </div>
     </div>
   );
