6428ebf3ff127c0e9d7d47bbeeefd5dcee622bbb
hommaryota <ryo4111016@gmail.com>
2023-11-22 00:48:26 +0900

    シャッフル機能追加

--- src/features/itemCardArea/ItemCardArea.tsx
+++ src/features/itemCardArea/ItemCardArea.tsx
@@ -15,30 +15,34 @@ const ItemCardArea: React.FC = () => {
   const [excludedItems, setExcludedItems] = useState(items);
   // 3つのアイテムを表示するuseState
   const [displayedItems, setDisplayedItems] = useState<DisplayedItems[]>([]);
+
+  // 初回レンダリング時に3つのアイテムを表示する
   useEffect(() => {
     const shuffledArray = _.sampleSize(excludedItems, 3);
     setDisplayedItems(shuffledArray);
   }, []);
 
+  // アイテムを削除する関数
   const deleteItem = (item: DisplayedItems, index: number) => {
     // 選ばれてない配列
     const newArray = _.differenceBy(excludedItems, displayedItems, "item");
-
     // // newArrayの中からランダムで1つ選ぶ
     const newItem = _.sampleSize(newArray, 1);
-
     // // 今回選ばれたアイテムを削除
     const newExcludedItems = _.differenceBy(newArray, newItem, "item");
-
     if (newExcludedItems.length === 0) return alert("アイテムがなくなりました");
-
     // 選んだアイテムと新しいアイテムを入れ替える
     const updatedCards = _.cloneDeep(displayedItems);
     updatedCards[index] = newItem[0];
-
     // stateを更新
     setDisplayedItems(updatedCards);
     setExcludedItems(newExcludedItems);
+  };
+
+  const shuffle = () => {
+    const newArray = _.differenceBy(excludedItems, displayedItems, "item");
+    const newItem = _.sampleSize(newArray, 3);
+    setDisplayedItems(newItem);
   };
 
   return (
@@ -53,7 +57,7 @@ const ItemCardArea: React.FC = () => {
           />
         ))}
       </div>
-      <Button onClick={() => deleteItem(items[0], 0)} />
+      <Button onClick={() => shuffle()} />
     </>
   );
 };
