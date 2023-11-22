00445a1d3311575daae245190230d326ad7dada8
hommaryota <ryo4111016@gmail.com>
2023-11-22 18:26:06 +0900

    fontSetカスタムフック化・FOUC対策

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
-import {useCallback, useEffect} from "react";
 import BgImage from "./components/bgImage/BgImage";
 import Title from "./components/title/Title";
 import Layout from "./components/layout/Layout";
 import CardArea from "./components/cardArea/CardArea";
 import ItemCardArea from "./features/itemCardArea/ItemCardArea";
 import EventCardArea from "./features/eventCardArea/EventCardArea";
+import useFontSet from "./hooks/useFontSet";
+import {useLayoutEffect} from "react";
 
 const App: React.FC = () => {
-  const fontSet = useCallback(() => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const ratio = width / height;
-    const htmlElement = document.querySelector("html")!;
-    if (ratio > 1.6) {
-      htmlElement.style.fontSize = Math.floor((1 / width) * 1000000) / 10000 + "vw";
-    }
-    if (ratio <= 1.6) {
-      htmlElement.style.fontSize =
-        Math.floor((1 / (height * 0.016)) * 10000) / 10000 + "vw";
-    }
-  }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", fontSet);
-    window.addEventListener("load", fontSet);
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", fontSet);
-      window.removeEventListener("load", fontSet);
-    };
+  const fontSet = useFontSet();
+  useLayoutEffect(() => {
+    fontSet();
   }, [fontSet]);
 
   return (
